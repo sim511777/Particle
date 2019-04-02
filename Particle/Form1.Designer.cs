188afe8707cfb7dad45336e673d3e0141473a547
sim511777 <sim51177@gmail.com>
2019-04-02 13:21:21 +0900

    Inital upload

---
+++ Particle/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Particle {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbxDraw = new System.Windows.Forms.PictureBox();
+            this.tmr17 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxDraw
+            // 
+            this.pbxDraw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbxDraw.Location = new System.Drawing.Point(0, 0);
+            this.pbxDraw.Name = "pbxDraw";
+            this.pbxDraw.Size = new System.Drawing.Size(1027, 661);
+            this.pbxDraw.TabIndex = 0;
+            this.pbxDraw.TabStop = false;
+            this.pbxDraw.Paint += new System.Windows.Forms.PaintEventHandler(this.pbxDraw_Paint);
+            this.pbxDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxDraw_MouseMove);
+            // 
+            // tmr17
+            // 
+            this.tmr17.Enabled = true;
+            this.tmr17.Interval = 10;
+            this.tmr17.Tick += new System.EventHandler(this.tmr17_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 661);
+            this.Controls.Add(this.pbxDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDraw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxDraw;
+        private System.Windows.Forms.Timer tmr17;
+    }
+}
+
---
+++ Particle/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Particle {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        List<Star> stars = new List<Star>();
+
+        private void AddStar(Point pt)
+        {
+            Star star = new Star(pt, rnd);
+            stars.Add(star);
+        }
+
+        private void DrawStar(Graphics g, Brush brush, float x, float y, float size)
+        {
+            g.FillEllipse(brush, x - size / 2, y - size / 2, size, size);
+        }
+
+        private void UpdateFrame(DateTime now)
+        {
+            this.stars = stars.Where(star => 10 * Math.Pow(0.1, (now - star.birthTime).TotalSeconds) >= 1f).ToList();
+        }
+
+        private void RenderFrame(Graphics g, DateTime now)
+        {
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            foreach (var star in this.stars)
+            {
+                float dt = (float)(now - star.birthTime).TotalSeconds;
+                float x = star.pos.X + dt * 200 * star.dxRatio;
+                float y = star.pos.Y + dt * 200;
+                float size = 10 * (float)Math.Pow(0.1, dt);
+                this.DrawStar(g, star.brush, x, y, size);
+            }
+        }
+
+        private void pbxDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.AddStar(e.Location);
+        }
+
+        private void pbxDraw_Paint(object sender, PaintEventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            this.RenderFrame(e.Graphics, now);
+        }
+
+        private void tmr17_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            this.UpdateFrame(now);
+            this.pbxDraw.Refresh();
+        }
+    }
+
+    public class Star {
+        public PointF pos;
+        public DateTime birthTime;
+        public float dxRatio;
+        public Brush brush;
+        public Star(PointF pos, Random rnd)
+        {
+            this.pos = pos;
+            this.birthTime = DateTime.Now;
+            this.dxRatio = (float)(rnd.NextDouble() * 1 - 0.5);
+            this.brush = new SolidBrush(Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)));
+        }
+        ~Star()
+        {
+            this.brush.Dispose();
+        }
+    }
+}
